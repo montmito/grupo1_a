8e64602d5ea590739aa6bcc213bd787acb70cd0e
felipebns <131299435+felipebns@users.noreply.github.com>
2023-12-02 17:24:28 -0300

    pop up semi funcional

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     invest_img = document.querySelector("#invest_img")
     invest_img.addEventListener("mouseover", function(event){
-        img = document.createElement('img')
-        img.src = "img/investir_popup.jpg"
-        invest_img.appendChild(img)
+        img = document.createElement('img');
+        img.classList.add("add")
+        img.src = "img/investir_popup.jpg";
+        invest_img.appendChild(img);
+        console.log(invest_img)
+    })
+
+    invest_img.addEventListener("mouseout", function(event){
+        img = invest_img.querySelector('.add')
+        img.remove()
     })
 })
