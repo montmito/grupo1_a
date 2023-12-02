858d9fa09fd3fd21e6626ac07bce4a54c59a1cea
felipebns <131299435+felipebns@users.noreply.github.com>
2023-12-02 16:39:40 -0300

    primeiro teste

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    invest_img = document.querySelector("#invest_img")
+    invest_img.addEventListener("mouseover", function(event){
+        img = document.createElement('img')
+        img.src = "img/investir_popup.jpg"
+        invest_img.appendChild(img)
+    })
 })
