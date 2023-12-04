7437632dd597bfd877f545aef55bba33efb3b18e
felipebns <131299435+felipebns@users.noreply.github.com>
2023-12-04 10:39:35 -0300

    cielo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //Pop up cielo 
+    cielo_img = document.querySelector("#cielo_img")
+    cielo_img.addEventListener("mouseover", function(event){
+        img = document.createElement('img');
+        img.classList.add("pop_cielo")
+        img.src = "img/check_popup.jpg";
+        cielo_img.appendChild(img);
+    })
 
+    cielo_img.addEventListener("mouseout", function(event){
+        img = cielo_img.querySelector('.pop_cielo')
+        img.remove()
+    })
 })
