23ae0e111bb4d1ce25f3561c44d5773c26322561
felipebns <131299435+felipebns@users.noreply.github.com>
2023-12-03 23:34:49 -0300

    pop up funcional

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     invest_img = document.querySelector("#invest_img")
     invest_img.addEventListener("mouseover", function(event){
         img = document.createElement('img');
-        img.classList.add("add")
+        img.classList.add("pop_invs")
         img.src = "img/investir_popup.jpg";
         invest_img.appendChild(img);
         console.log(invest_img)
     })
 
     invest_img.addEventListener("mouseout", function(event){
-        img = invest_img.querySelector('.add')
+        img = invest_img.querySelector('.pop_invs')
         img.remove()
     })
 })
